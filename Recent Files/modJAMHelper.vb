a7c5226abaed245023884d929246c5bcfeb4be00
Cleek365 <abienfred.agarap@yahoo.com>
2015-08-02 16:31:11 +0800

    * showMessage() method was introduced * Shortened codes for changing menu visibility by using conditional expressions

--- Recent Files/modJAMHelper.vb
+++ Recent Files/modJAMHelper.vb
@@ -1,55 +1,76 @@
-﻿Imports System
-Imports System.IO
+﻿Imports System.IO
 Imports System.Security.AccessControl
 Module modJAMHelper
-    Friend Sub triggerMenu(isVisible As Boolean)
-        If isVisible Then
-            frmJAM.btnPassword.Visible = False
-            frmJAM.btnHelp.Visible = False
-            frmJAM.btnAbout.Visible = False
-            frmJAM.btnExit.Visible = False
-        ElseIf Not isVisible Then
-            frmJAM.btnPassword.Visible = True
-            frmJAM.btnHelp.Visible = True
-            frmJAM.btnAbout.Visible = True
-            frmJAM.btnExit.Visible = True
-        End If
+    Friend Sub showMessage(message As String)
+        hideAllMenu()
+        frmMessage.lblMessage.Text = message
+        frmMessage.Show()
     End Sub
+
+    Friend Sub triggerMenu(isVisible As Boolean)
+        ' Shortened code using conditional expression
+        frmJAM.btnPassword.Visible = If(isVisible, False, True)
+        frmJAM.btnHelp.Visible = If(isVisible, False, True)
+        frmJAM.btnAbout.Visible = If(isVisible, False, True)
+        frmJAM.btnExit.Visible = If(isVisible, False, True)
+
+        ' Long code using If-Else statements
+        'If isVisible Then
+        '    frmJAM.btnPassword.Visible = False
+        '    frmJAM.btnHelp.Visible = False
+        '    frmJAM.btnAbout.Visible = False
+        '    frmJAM.btnExit.Visible = False
+        'ElseIf Not isVisible Then
+        '    frmJAM.btnPassword.Visible = True
+        '    frmJAM.btnHelp.Visible = True
+        '    frmJAM.btnAbout.Visible = True
+        '    frmJAM.btnExit.Visible = True
+        'End If
+    End Sub
+
     Friend Sub triggerSubMenu(isVisible As Boolean)
-        If isVisible Then
-            frmJAM.btnEditPassword.Visible = False
-            frmJAM.btnAddPassword.Visible = False
-            frmJAM.btnRemovePassword.Visible = False
-        ElseIf Not isVisible Then
-            frmJAM.btnEditPassword.Visible = True
-            frmJAM.btnAddPassword.Visible = True
-            frmJAM.btnRemovePassword.Visible = True
-        End If
+        ' Shortened code using conditional expression
+        frmJAM.btnEditPassword.Visible = If(isVisible, False, True)
+        frmJAM.btnAddPassword.Visible = If(isVisible, False, True)
+        frmJAM.btnRemovePassword.Visible = If(isVisible, False, True)
+
+        ' Long code using If-Else statements
+        'If isVisible Then
+        '    frmJAM.btnEditPassword.Visible = False
+        '    frmJAM.btnAddPassword.Visible = False
+        '    frmJAM.btnRemovePassword.Visible = False
+        'ElseIf Not isVisible Then
+        '    frmJAM.btnEditPassword.Visible = True
+        '    frmJAM.btnAddPassword.Visible = True
+        '    frmJAM.btnRemovePassword.Visible = True
+        'End If
     End Sub
 
     Friend Sub hideAllMenu()
         triggerMenu(True)
         triggerSubMenu(True)
     End Sub
-    
+
     Friend Sub addPassword()
         hideAllMenu()
-        If Not File.Exists(Directory.GetCurrentDirectory & "\index.txt") Then ' check if password file does not exist
-            frmJAM.Hide()
+        If Not System.IO.File.Exists(System.IO.Directory.GetCurrentDirectory & "\index.txt") Then ' check if password file does not exist
             frmSet.txtPass.Clear()
             frmSet.Show() ' frmSet is the form for setting password
         Else
-            MessageBox.Show("You already have a password!", "JAM Folder Protector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            showMessage("You already have a password!")
         End If
     End Sub
+
     Friend Sub removePassword()
-        If File.Exists(Directory.GetCurrentDirectory & "\index.txt") Then
-            File.Delete(Directory.GetCurrentDirectory & "\index.txt") ' index.txt contains the password
-            MessageBox.Show("Password deleted!", "JAM", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        hideAllMenu()
+        If System.IO.File.Exists(System.IO.Directory.GetCurrentDirectory & "\index.txt") Then
+            System.IO.File.Delete(System.IO.Directory.GetCurrentDirectory & "\index.txt") ' index.txt contains the password
+            showMessage("Password deleted!")
         Else
-            MessageBox.Show("There is no password to delete!", "JAM Folder Protector", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            showMessage("There is no password to delete!")
         End If
     End Sub
+
     Friend Sub browseForFolder()
         If frmJAM.fbdJAM.ShowDialog = Windows.Forms.DialogResult.OK Then
             frmJAM.txtPath.Text = frmJAM.fbdJAM.SelectedPath
@@ -63,6 +84,7 @@ Module modJAMHelper
             End If
         End If
     End Sub
+
     Friend Sub loadRecentFiles()
         If File.Exists(Directory.GetCurrentDirectory & "\recent.txt") Then ' checks if there is a list of recent folders
             For Each line As String In File.ReadAllLines(Directory.GetCurrentDirectory & "\recent.txt")
@@ -70,28 +92,31 @@ Module modJAMHelper
             Next
         End If
     End Sub
+
     Friend Sub lockFolder(path As String)
         Try
             File.SetAttributes(path, FileAttributes.Hidden) ' set the file attribute of selected folder to hidden
             Dim fs = System.IO.File.GetAccessControl(path)
             fs.AddAccessRule(New FileSystemAccessRule(Environment.UserName, FileSystemRights.FullControl, AccessControlType.Deny))
             File.SetAccessControl(path, fs)
-            MessageBox.Show(path + " is now locked!", "JAM Folder Protector", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            showMessage(path + vbCrLf + "is now locked!")
         Catch ex As Exception
-            MessageBox.Show(ex.ToString)
+            showMessage("An unexpected error occurred.")
         End Try
     End Sub
+
     Friend Sub unlockFolder(path As String)
         Try
             Dim fs As FileSystemSecurity = File.GetAccessControl(path)
             fs.RemoveAccessRule(New FileSystemAccessRule(Environment.UserName, FileSystemRights.FullControl, AccessControlType.Deny))
             File.SetAccessControl(path, fs)
             File.SetAttributes(path, FileAttributes.Normal)
-            MessageBox.Show(path + " is now unlocked!", "JAM Folder Protector", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            showMessage(path + vbCrLf + "is now unlocked!")
         Catch ex As Exception
-            MessageBox.Show(ex.ToString)
+            showMessage("An unexpected error occurred.")
         End Try
     End Sub
+
     Friend Sub CreateWrite()
         Using sw As StreamWriter = File.CreateText(Directory.GetCurrentDirectory & "\recent.txt") ' sw: local variable
             sw.WriteLine(frmJAM.fbdJAM.SelectedPath)
@@ -99,6 +124,7 @@ Module modJAMHelper
             sw.Close()
         End Using
     End Sub
+
     Friend Sub ExistWrite()
         For Each line As String In File.ReadAllLines(Directory.GetCurrentDirectory & "\recent.txt")
             If Not line = frmJAM.fbdJAM.SelectedPath Then
