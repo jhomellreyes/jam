968adc835e4b258aed64cb715430385274fc3cc7
Cleek365 <abienfred.agarap@yahoo.com>
2015-08-02 15:05:50 +0800

    * Re-designed UI * Shortened codes by using module

--- Recent Files/frmEdit.Designer.vb
+++ Recent Files/frmEdit.Designer.vb
@@ -34,10 +34,11 @@ Partial Class frmEdit
         'btnClear
         '
         Me.btnClear.BackColor = System.Drawing.Color.LightGray
+        Me.btnClear.Cursor = System.Windows.Forms.Cursors.Hand
         Me.btnClear.FlatAppearance.BorderSize = 0
         Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnClear.Font = New System.Drawing.Font("Segoe UI", 11.0!)
-        Me.btnClear.Location = New System.Drawing.Point(369, 51)
+        Me.btnClear.Location = New System.Drawing.Point(378, 55)
         Me.btnClear.Name = "btnClear"
         Me.btnClear.Size = New System.Drawing.Size(112, 33)
         Me.btnClear.TabIndex = 5
@@ -47,10 +48,11 @@ Partial Class frmEdit
         'btnEnter
         '
         Me.btnEnter.BackColor = System.Drawing.Color.LightGray
+        Me.btnEnter.Cursor = System.Windows.Forms.Cursors.Hand
         Me.btnEnter.FlatAppearance.BorderSize = 0
         Me.btnEnter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnEnter.Font = New System.Drawing.Font("Segoe UI", 11.0!)
-        Me.btnEnter.Location = New System.Drawing.Point(369, 12)
+        Me.btnEnter.Location = New System.Drawing.Point(378, 12)
         Me.btnEnter.Name = "btnEnter"
         Me.btnEnter.Size = New System.Drawing.Size(112, 33)
         Me.btnEnter.TabIndex = 4
@@ -60,8 +62,9 @@ Partial Class frmEdit
         'Label2
         '
         Me.Label2.AutoSize = True
+        Me.Label2.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Label2.Font = New System.Drawing.Font("Segoe UI", 11.0!)
-        Me.Label2.Location = New System.Drawing.Point(92, 56)
+        Me.Label2.Location = New System.Drawing.Point(30, 61)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(109, 20)
         Me.Label2.TabIndex = 8
@@ -71,29 +74,32 @@ Partial Class frmEdit
         '
         Me.txtNPass.BackColor = System.Drawing.Color.White
         Me.txtNPass.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtNPass.Cursor = System.Windows.Forms.Cursors.Hand
         Me.txtNPass.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtNPass.Location = New System.Drawing.Point(207, 55)
+        Me.txtNPass.Location = New System.Drawing.Point(145, 60)
         Me.txtNPass.Name = "txtNPass"
         Me.txtNPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
-        Me.txtNPass.Size = New System.Drawing.Size(127, 22)
+        Me.txtNPass.Size = New System.Drawing.Size(218, 22)
         Me.txtNPass.TabIndex = 9
         '
         'txtCPass
         '
         Me.txtCPass.BackColor = System.Drawing.Color.White
         Me.txtCPass.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtCPass.Cursor = System.Windows.Forms.Cursors.Hand
         Me.txtCPass.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtCPass.Location = New System.Drawing.Point(207, 16)
+        Me.txtCPass.Location = New System.Drawing.Point(145, 17)
         Me.txtCPass.Name = "txtCPass"
         Me.txtCPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
-        Me.txtCPass.Size = New System.Drawing.Size(127, 22)
+        Me.txtCPass.Size = New System.Drawing.Size(218, 22)
         Me.txtCPass.TabIndex = 7
         '
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 11.0!)
-        Me.Label1.Location = New System.Drawing.Point(74, 17)
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(127, 20)
         Me.Label1.TabIndex = 6
@@ -103,7 +109,7 @@ Partial Class frmEdit
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(194, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(57, Byte), Integer))
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(158, Byte), Integer), CType(CType(226, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(502, 100)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.txtNPass)
